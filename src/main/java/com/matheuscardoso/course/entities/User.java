dfadb69ba8026fb22f023dd50dec945815416523
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-01-23 21:33:06 -0300

    Adding User entity

---
+++ src/main/java/com/matheuscardoso/course/entities/User.java
@@ -0,0 +1,10 @@
+package com.matheuscardoso.course.entities;
+
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+}
